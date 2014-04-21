49fae735be444bcbd82356e40ac8d44fc4634757
@yepesasecas <yepes07@gmail.com>
2014-04-21 12:00:35 -0500

    Group class created

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140421164858) do
+
+  create_table "groups", force: true do |t|
+    t.string   "name"
+    t.integer  "leader_id"
+    t.integer  "companion_one_id"
+    t.integer  "companion_two_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
