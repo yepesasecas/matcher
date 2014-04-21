3118bf64b670fc7ef9886e800407c1d66ba0dc22
@yepesasecas <yepes07@gmail.com>
2014-04-21 14:40:34 -0500

    User has_many associations working properly

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ActiveRecord::Base
 
-  validates :name, presence: true
   belongs_to :leader, class_name: "User", foreign_key: "leader_id"
   belongs_to :companion_one, class_name: "User", foreign_key: "companion_one_id"
   belongs_to :companion_two, class_name: "User", foreign_key: "companion_two_id"
+  validates :name, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,22 @@ class UserTest < ActiveSupport::TestCase
     user = User.new
     assert_not user.save, "the name of the user must be present"
   end
+
+  test "User has many leader Groups" do
+    user = User.create name: "Ricardo"
+    group = Group.create name: "test", leader_id: user.id
+    assert_equal user.leader_groups.first.name, group.name, "leader_groups from users doesnt work" 
+  end
+
+  test "User has many companion_one Groups" do
+    user = User.create name: "Ricardo"
+    group = Group.create name: "test", companion_one_id: user.id
+    assert_equal user.companion_one_groups.first.name, group.name, "companion_one_groups from users doesnt work" 
+  end
+
+  test "User has many companion_two Groups" do
+    user = User.create name: "Ricardo"
+    group = Group.create name: "test", companion_two_id: user.id
+    assert_equal user.companion_two_groups.first.name, group.name, "companion_two_groups from users doesnt work" 
+  end
 end
